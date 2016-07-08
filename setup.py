5b5ed70de67e18adfab85d33e6e2d865c3df36be
Chris Calderon <calderon.christian760@gmail.com>
2016-07-08 16:24:47 -0700

    Switched to universal setup.py + config.json

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
 from setuptools import setup
+import json
+
+with open('config.json') as cfg:
+    config = json.load(cfg)
+
+
+def is_http(req):
+    """Checks if a requirement is a link."""
+    return req.startswith('http://') or req.startswith('https://')
+
+
+def dep_sorter(rs, r):
+    """Appends the requirement r to the proper list."""
+    print rs, r
+    if is_http(r):
+        rs[1].append(r)
+    else:
+        rs[0].append(r)
+    return rs
 
 with open('requirements.txt') as reqs:
-    requirements = filter(None, [r.strip() for r in reqs])
+    requirements, links = reduce(dep_sorter,
+                                 filter(None,
+                                        [r.strip() for r in reqs]),
+                                 [[],[]])
 
-setup(name='rpctools',
-      version='1.0dev1',
-      description='JSON RPC Classes for Ethereum Nodes.',
-      author='ChrisCalderon',
-      author_email='pythonwiz@protonmail.com',
-      packages=['rpctools'],
-      install_requires=requirements)
+config['install_requires'] = requirements
+config['dependency_links'] = links
+setup(**config)
